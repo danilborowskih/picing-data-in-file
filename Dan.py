a67322c81ad8daab24968cc66daebce88e075b9a
danilborowskih <DedovobeD@yandex.ru>
2021-10-05 23:35:29 +0800

    sep_ex раскидан по функциям но без numpy

--- Dan.py
+++ Dan.py
@@ -1,49 +1,68 @@
 import pickle , os
 import timeit
+#эта версия в функциях но без numpy
+class MyClass:
 
-def sep_exe(func, num_of_members, num_of_failes):
-        results_in_one_file = num_of_members // num_of_failes  # результ в одном файле
-        iterator_for_start_value = 0  # указывает номер переменной с которой начнется func
-        file_iterator = 0  # указывает номер файла
-        try:
-            while True: # проверка существования файла
-                file_name = 'dan/data_'+str(file_iterator)+'.pkl'
-                with open(file_name, 'rb') as f:
+    def __init__(self,func,num_of_res,num_of_blocks):
+        self.func = func
+        self.res_num = num_of_res
+        self.dir_of_res = 'my_directory'
+        self.path_arr = [os.path.join(self.dir_of_res, f'{i}') for i in range(num_of_blocks)]
+        self.step = num_of_res // num_of_blocks  # результ в одном файле
+        self.num_of_blocks = num_of_blocks
+
+    def __len__(self):
+        return self.len
+    
+    def Load(self, path) -> list:# возвращает список pickl
+        with open(path, 'rb') as f:
+            return pickle.load(f)
+
+    def file_exist(self) -> int: # возвращает количество уже заполненных не поврежденных файлов
+        if not os.path.exists(self.dir_of_res):
+            os.mkdir(self.dir_of_res)
+            return 0
+        l = len(os.listdir(self.dir_of_res))
+        n = 0 
+        for i in range(l):
+            try:
+                with open(self.path_arr[i], 'rb') as f:
                     pickle.load(f)
-                file_iterator+=1
-                iterator_for_start_value+=results_in_one_file
-        except:
-            print("файлов с данными на момент начала записи ",file_iterator)
-      
-        while file_iterator < num_of_failes:
-            file_name='dan/data_'+str(file_iterator)+'.pkl' # name for file we read
+                n+=1
+            except:
+                break
+        return n
+    
+    def fill_results_in_list(self,file_iter)->list:
+        return [func_result for i, func_result in enumerate(self.func(file_iter*self.step))
+                if i < self.step or (file_iter == self.num_of_blocks - 1)]# здесь мы наполняем лист для записи его в файл
+                
+    def Dump(self,reslist,i):
+        with open(self.path_arr[i],'wb') as f:
+            pickle.dump(reslist,f)
 
-            res_list = [func_result for i, func_result in enumerate(func(iterator_for_start_value))
-            if i < results_in_one_file or (file_iterator == num_of_failes - 1)]# здесь мы наполняем лист для записи его в файл
-
+    def sep_exe(self):
+            file_iterator = MyClass.file_exist(self)  # указывает номер файла который будем заполнять
+            iterator_for_start_value = file_iterator*self.step  # указывает номер переменной с которой начнется func
+            print(file_iterator)
+            while file_iterator < self.num_of_blocks:
+                res_list = MyClass.fill_results_in_list(self,file_iterator)
+                MyClass.Dump(self,res_list,file_iterator)  # записываем в файл
+                file_iterator += 1  # считаем уже заполненные файлы
+    
+    def load_compl(self):
+        file_iter = MyClass.file_exist(self)
+        res_list=[]
+        for i in range(file_iter) :
+            res_list = MyClass.Load(self,self.path_arr[i])
             print(res_list)
-            with open(file_name, 'wb') as f:
-                pickle.dump(res_list, f)  # записываем в файл
-                f.close()
-            
-            # считаем сколько уже записанно элементов
-            iterator_for_start_value += len(res_list)
-            file_iterator += 1  # считаем уже заполненные файлы
 
-    # могу не использовать количество файлов (как скажешь семпай)
-def load_compl(a):
-        try:
-            file_iterator = 0 # number of files was read
-            one_file_data_list = []
-            data_list = []
-            while a :
-                file_name='dan/data_'+str(file_iterator)+'.pkl' 
-                with open(file_name, 'rb') as f:                
-                    one_file_data_list=(pickle.load(f))
-                file_iterator += 1
-                data_list.extend(one_file_data_list)
-        except IndexError:  # ошибка чтения 
-            print("ошибка при чтении")
-        finally:
-            print("всего файлов с данными", file_iterator)
-            return data_list  # возвращаем данные
+
+
+def some_func(start):
+    for i in range(start,33):
+        yield i**2
+if __name__=='__main__':
+    test = MyClass(some_func,33,5)
+    test.sep_exe()
+    test.load_compl()
